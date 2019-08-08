3cba7c914770cb07ed5976c6bd94f3487e5c374d
Sam Stites <stites@users.noreply.github.com>
2019-08-08 14:14:24 -0400

    use probtorch-dev

--- probtorch/default.nix
+++ probtorch/default.nix
@@ -7,8 +7,13 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "probtorch";
     repo = "probtorch";
-    rev = "4b7e1404354c05401fc4132c9cbe0a7e1ac52eb5";
-    sha256= "1dgrvi0pjnxinaw7r5q9d3gyd40qn3dknm15yygbl105ybaym9i6";
+    # MASTER
+    # rev = "4b7e1404354c05401fc4132c9cbe0a7e1ac52eb5";
+    # sha256= "1dgrvi0pjnxinaw7r5q9d3gyd40qn3dknm15yygbl105ybaym9i6";
+
+    # DEVELOPMENT
+    rev = "34173ffa36f5c85a160c5522523a38deb62b38a8";
+    sha256 = "0pp8iv02jwwacpmnyndwdmq994ymaahd0mpsw5rpdhlh21xdsnpw";
   };
 
   doCheck = false;
