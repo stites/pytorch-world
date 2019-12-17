a78ba060e2e5afc7635db1f198cf290fb267e7f2
Sam Stites <stites@users.noreply.github.com>
2019-12-17 15:14:19 -0500

    add in testshell for 1.4.0a0

--- pytorch/release.nix
+++ pytorch/release.nix
@@ -1,39 +1,14 @@
 { pkgs ? import ../pin/nixpkgs.nix {}, python ? pkgs.python3 }:
 
 with pkgs.lib.attrsets;
+with builtins;
 
 let
   protobuf = pkgs.protobuf;
-  generic = args: unstableGeneric null args;
-  unstableGeneric = pinpath: args:
-    let
-      unstable = builtins.fromJSON (builtins.readFile pinpath);
-      mypython = python.override {
-        packageOverrides = self: super: rec {
-          numpy = if (hasAttr "mklSupport" args && args.mklSupport)
-                  then super.numpy.override { blas = pkgs.mkl; }
-                  else super.numpy;
-          stablept = (self.callPackage ./. ({ } // args));
-          pytorch =
-            if pinpath == null then stablept
-            else stablept.overrideAttrs(old: {
-              version = unstable.rev;
-              doCheck = true; # always run tests on unstable packages
-              src = pkgs.fetchFromGitHub {
-                owner  = "pytorch";
-                repo   = "pytorch";
-                rev    = unstable.rev;
-                fetchSubmodules = true;
-                sha256 = unstable.sha256;
-              };
-            });
-        };
-        self = mypython;
-      };
-    in mypython.withPackages(ps: [ ps.pytorch ]);
+  inherit (import ./generic.nix { inherit pkgs python; }) generic unstableGeneric;
 
 in rec {
-  pytorch-unstable = unstableGeneric ./unstable-pins/2019-12-12.json {
+  pytorch-unstable = unstableGeneric ./unstable-pins/v1.4.0a0-2019-12-17.json "1.4.0a0" {
       mklSupport = false;
       cudaSupport = false;
       buildBinaries = false;
